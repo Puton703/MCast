0ebc6336f315448650fb50bd387f90d82e8c1f07
puton703 <naosanputon@gmail.com>
2018-12-12 00:15:57 +0900

    add main window

--- mcast.py
+++ mcast.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import glob
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
@@ -10,24 +11,37 @@ from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent, QMediaPlaylist
 from PyQt5.QtMultimediaWidgets import QVideoWidget
 
 
-class MainWindow(QWidget):
+class BackWindow(QWidget):
     def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent)
+        super(BackWindow, self).__init__(parent)
 
         self.init_ui()
 
+        self.w = None
+
         self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        self.main_mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+
         # self.mediaPlayer.stateChanged.connect(self.mediaStateChanged)
         self.mediaPlayer.positionChanged.connect(self.positionChanged)
         self.mediaPlayer.durationChanged.connect(self.durationChanged)
         self.mediaPlayer.error.connect(self.handleError)
 
-        self._playlist = QMediaPlaylist()
-        self.addMedia("movie/movie1.wmv")
-        self._stopped = True
+        self.main_mediaPlayer.positionChanged.connect(self.positionChanged)
+        self.main_mediaPlayer.durationChanged.connect(self.durationChanged)
+        self.main_mediaPlayer.error.connect(self.handleError)
 
+        self._playlist = QMediaPlaylist()
+        #self.addMedia("movie/movie1.wmv")
+
+        self.init_play_list()
+
+        self._stopped = True
         self.mediaPlayer.setVideoOutput(self.ui.videoWidget)
+
+
         self.mediaPlayer.setPlaylist(self._playlist)
+        self.main_mediaPlayer.setPlaylist(self._playlist)
 
     def init_ui(self):
         ui_path = "ui/MediaPlayer.ui"
@@ -35,9 +49,22 @@ class MainWindow(QWidget):
         self.ui = loadUi(ui_path, self)
 
         self.ui.playButton.clicked.connect(self.play_movie)
+        self.ui.nextButton.clicked.connect(self.next_movie)
+        self.ui.launchButton.clicked.connect(self.launch_window)
 
         self.ui.positionSlider.setRange(0, 0)
         self.ui.positionSlider.sliderMoved.connect(self.setPosition)
+
+
+    def init_play_list(self):
+        movies = glob.glob(os.path.dirname("__file__") +  "movie/*")
+
+        #sort movies
+
+        for movie in movies:
+            self.addMedia(movie)
+
+            self.ui.listWidget.addItem(movie)
 
     # プレイリストに動画を追加
     def addMedia(self, media_file):
@@ -57,12 +84,28 @@ class MainWindow(QWidget):
             # self.mediaPlayer.setVideoOutput(self.ui.videoWidget)
             # self.mediaPlayer.setPlaylist(self._playlist)
             self.mediaPlayer.play()
+            self.main_mediaPlayer.play()
             self._stopped = False
             self.ui.playButton.setText("pause")
         else:
             self.mediaPlayer.pause()
+            self.main_mediaPlayer.pause()
+
             self._stopped = True
             self.ui.playButton.setText("play")
+
+    def next_movie(self):
+        self._playlist.next()
+
+    def back_movie(self):
+        pass
+
+    def launch_window(self):
+        print "aaaaaaaaa"
+        self.w = MainWindow()
+        #self.w.show()
+
+        self.main_mediaPlayer.setVideoOutput(self.w.ui.videoWidget)
 
     def positionChanged(self, position):
         self.ui.positionSlider.setValue(position)
@@ -72,15 +115,40 @@ class MainWindow(QWidget):
 
     def setPosition(self, position):
         self.mediaPlayer.setPosition(position)
+        self.main_mediaPlayer.setPosition(position)
 
     def handleError(self):
         self.playButton.setEnabled(False)
         self.errorLabel.setText("Error: " + self.mediaPlayer.errorString())
 
 
+class MainWindow(QWidget):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        #self.button = QPushButton("test", self)
+
+        self.ui = QWidget(parent)
+        #self.ui.button = QPushButton("test", self)
+        self.ui.videoWidget = QVideoWidget()
+        layout = QHBoxLayout(self)
+        layout.addWidget(self.ui.videoWidget)
+        self.ui.setLayout(layout)
+
+        self.ui.show()
+
+
+        #self.setCentralWidget(self.button)
+
+        #self.show()
+
+
+
+
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    main_window = MainWindow()
-    main_window.show()
+    window = BackWindow()
+    window.show()
 
     sys.exit(app.exec_())
